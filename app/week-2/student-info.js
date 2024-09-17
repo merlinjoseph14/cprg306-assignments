d4f0cfcb62e4136cc7bdda4edcab9e3608da4052
merlinjoseph14 <merlinjjoseph014@gmail.com>
2024-09-17 12:16:11 -0600

    created student-into.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+export default function Studentnfo() {
+    return (
+      <main>
+        <h1>Merlin Joseph</h1>
+        <h1>
+            Github repository:
+            <a href="https://github.com/merlinjoseph14/cprg306-assignments.git">
+            </a>
+        </h1>
+      </main>
+    );
+  }
