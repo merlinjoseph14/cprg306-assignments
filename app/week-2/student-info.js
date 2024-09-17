76c506e3c815f59c06ff96cdeab9c07a7488de22
merlinjoseph14 <merlinjjoseph014@gmail.com>
2024-09-17 13:07:47 -0600

    update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -5,6 +5,7 @@ export default function Studentnfo() {
         <h1>
             Github repository:
             <a href="https://github.com/merlinjoseph14/cprg306-assignments.git">
+                Link to repository            
             </a>
         </h1>
       </main>
