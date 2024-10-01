5c78908ec9999e22480e87b90d1a9616b9aa597e
merlinjoseph14 <merlinjjoseph014@gmail.com>
2024-09-30 20:35:55 -0600

    week 3 assignment updates

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Page() {
 
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <main className="flex flex-col justify-center items-center p-5">
+      <h1 className="text-2xl font-bold mb-4">CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="/week-2/">Week 2 Assignment</Link>
+        <Link href="/week-2/" className="hover:text-green-500 text-xl">Week 2</Link>
       </p>
       <p>
-        <Link href="/week-3/">Week 3 Assignment</Link>
+        <Link href="/week-3/" className="hover:text-green-500 text-xl">Week 3</Link>
       </p>
     </main>
   );
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 
 const Page =  () => {
     return (
-        <main className="px-5">
+        <main className="p-5">
             <h1 className="text-4xl font-bold mb-5">Shopping List</h1>
             <ItemList/>
         </main>
